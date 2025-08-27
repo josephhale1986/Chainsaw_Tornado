22614acfa3c54ffae41d0fe37d9ee941c73af074
josephhale1986 <144393008+josephhale1986@users.noreply.github.com>
2025-08-26 21:50:03 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('ct-final-fix').then(c=>c.addAll([
-    './','./index.html','./game.js','./manifest.webmanifest',
-    './assets/bgm_main.wav','./assets/bgm_boss.wav','./assets/menu.png'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
