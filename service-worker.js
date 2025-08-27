1ae42df9e7968489ecaf6a6d965761adbfc9f929
josephhale1986 <144393008+josephhale1986@users.noreply.github.com>
2025-08-27 13:34:17 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('ct-atlas-v3').then(c=>c.addAll(['./','./index.html','./game.js','./manifest.webmanifest'])));
-});
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
