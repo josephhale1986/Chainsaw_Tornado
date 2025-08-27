5557e304e7aed145e4101540b1898223e7efd69f
josephhale1986 <144393008+josephhale1986@users.noreply.github.com>
2025-08-26 22:42:41 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('ct-atlas-v3').then(c=>c.addAll(['./','./index.html','./game.js','./manifest.webmanifest'])));
+});
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
